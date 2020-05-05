2935a8bb9a329d1904ae0906ec1375e32fe1c03b
barry-ha <barry@electromagneticsoftware.com>
2020-05-05 11:26:25 -0700

    move three buttons toward bottom of screen, making more room for title and "next screen" touch

--- view_volume.cpp
+++ view_volume.cpp
@@ -1,52 +1,36 @@
 /* File: view_volume.cpp
 
-  This is the user interface to increase/decrease/mute speaker volume.
 
-     +-----------------+------------+
-     | Audio           |            |
-     | Volume          |    Up      |
-     |                 |            |
-     | 3 of 10         |            |
-     |                 +------------+
-     |                 |            |
-     +--------+        |   Down     |
-     |  Mute  |        |            |
-     |        |        |            |
-     +--------+--------+------------+
+  Software: Barry Hansen, K7BWH, barry@k7bwh.com, Seattle, WA
+  Hardware: John Vanderbeck, KM7O, Seattle, WA
 
-  This "view" module is all about speaker volume.
-  As such, the only thing it does is set the DS1804 digital potentiometer.
-  It has nothing to do with the DAC, audio files, Morse code speed/pitch, or other playback attributes.
+  Purpose:  This is the user interface to increase/decrease/mute speaker volume.
+            This "view" module is all about speaker volume.
+            As such, the only thing it does is set the DS1804 digital potentiometer.
+            It has nothing to do with the DAC, audio files, Morse code speed/pitch, or other playback attributes.
+
+            +---------------------------------+
+            |    33    Audio Volume           |... yRow1
+            |    33    of 10                  |... yRow2
+            |                                 |
+            |      +---------------+          |
+            |      |      Up       |          |
+            |      |               | +--------+
+            |      +---------------+ |  Mute  |
+            |      |     Down      | +--------+
+            |      |               |          |
+            +------+---------------+----------+
+
 */
 
 #include <Arduino.h>
 #include "Adafruit_GFX.h"         // Core graphics display library
 #include "Adafruit_ILI9341.h"     // TFT color display library
-#include "constants.h"            // Griduino constants and colors
+#include "constants.h"            // Griduino constant declarations
 #include "model.cpp"              // "Model" portion of model-view-controller
 #include "morse_dac.h"            // morse code
 #include "DS1804.h"               // DS1804 digital potentiometer library
 #include "save_restore.h"         // save/restore configuration data to SDRAM
-
-// ========== extern ===========================================
-extern Adafruit_ILI9341 tft;      // Griduino.ino
-extern void printProportionalText(int xx, int yy, String text, uint16_t cc);
-extern void erasePrintProportionalText(int xx, int yy, int ww, String text, uint16_t cc);
-extern DACMorseSender dacMorse;   // morse code (so we can send audio samples)
-extern DS1804 volume;             // digital potentiometer
-
-void showNameOfView(String sName, uint16_t fgd, uint16_t bkg);  // Griduino.ino
-void initFontSizeSmall();         // Griduino.ino
-void initFontSizeBig();           // Griduino.ino
-int getOffsetToCenterText(String text); // Griduino.ino
-
-// ========== forward reference ================================
-void updateVolumeScreen();
-void volUp();
-void volDown();
-void volMute();
-int loadConfigVolume();
-void saveConfigVolume();
 
 // ------------ typedef's
 typedef struct {
@@ -64,19 +48,42 @@ typedef struct {
 typedef void (*simpleFunction)();
 typedef struct {
   char text[26];
-  int x;
-  int y;
-  int w;
-  int h;
+  int x, y;
+  int w, h;
   int radius;
   uint16_t color;
   simpleFunction function;
 } Button;
 
+// ========== global scope =====================================
+int gVolIndex = 5;          // init to middle value
+int gPrevVolIndex = -1;     // remembers previous volume setting to avoid erase/write the same value
+
+
+// ========== extern ===========================================
+extern Adafruit_ILI9341 tft;      // Griduino.ino
+extern void printProportionalText(int xx, int yy, String text, uint16_t cc);
+extern void erasePrintProportionalText(int xx, int yy, int ww, String text, uint16_t cc); // view_status.cpp
+extern DACMorseSender dacMorse;   // morse code (so we can send audio samples)
+extern DS1804 volume;             // digital potentiometer
+
+//id showNameOfView(String sName, uint16_t fgd, uint16_t bkg);  // Griduino.ino
+void initFontSizeSmall();         // Griduino.ino
+void initFontSizeBig();           // Griduino.ino
+int getOffsetToCenterText(String text); // Griduino.ino
+
+// ========== forward reference ================================
+void updateVolumeScreen();
+void volUp();
+void volDown();
+void volMute();
+int loadConfigVolume();
+void saveConfigVolume();
+
 // ========== constants ===============================
-const int yRow1 = 32;
-const int yRow2 = yRow1 + 32;
-const int yRow3 = yRow2 + 48;
+const int yRow1 = 32;             // label: "Audio Volume"
+const int yRow2 = yRow1 + 30;     // text:  "4 of 10"
+//nst int yRow3 = yRow2 + 20;
 //nst int yRow4 = yRow3 + 44;
 //nst int yRow5 = yRow4 + 44;
 //nst int yRow6 = yRow5 + 44;
@@ -84,19 +91,20 @@ const int yRow3 = yRow2 + 48;
 const int labelX = 8;       // indent labels, slight margin to left edge of screen
 const int valueX = 124;     // indent values
 
-const int numLabels = 5;
+const int numLabels = 2;
 Label volLabels[numLabels] = {
-  {"Audio",  labelX, yRow1, cLABEL},
-  {"Volume", labelX, yRow2, cLABEL},
+  //   text          x,y      color  
+  {"Audio Volume",  64,yRow1, cLABEL},
+  {"of 10",         64,yRow2, cLABEL},
 };
 const int nVolButtons = 3;
 const int margin = 10;      // slight margin between button border and edge of screen
 const int radius = 10;      // rounded corners
 Button volButtons[nVolButtons] = {
-  // text      x,y           w,h        r      color
-  {"",       160, margin,  152, 106,  radius, cBUTTONLABEL, volUp  }, // Up
-  {"",       160, 126,     152, 106,  radius, cBUTTONLABEL, volDown}, // Down
-  {"Mute", margin,140,     120,  92,  radius, cBUTTONLABEL, volMute}, // Mute
+  // text     x,y        w,h        r      color
+  {"",       50, 80,   140,74,  radius, cBUTTONLABEL, volUp  }, // Up
+  {"",       50,158,   140,75,  radius, cBUTTONLABEL, volDown}, // Down
+  {"Mute",  220,118,    90,64,  radius, cBUTTONLABEL, volMute}, // Mute
 };
 int volLevel[11] = {
   // Digital potentiometer settings, about 2 dB steps = ratio 1.585
@@ -113,18 +121,13 @@ int volLevel[11] = {
   /*10 */ 99,   // max allowed wiper position
 };
 
-// ========== globals =================================
-int gVolIndex = 5;          // init to middle value
-int gPrevVolIndex = -1;     // remembers previous volume setting to avoid erase/write the same value
-
-
 // ========== helpers =================================
 void setVolume(int volIndex) {
   // set digital potentiometer
   // @param wiperPosition = 0..10
   int wiperPosition = volLevel[ volIndex ];
   volume.setWiperPosition( wiperPosition );
-  //~Serial.print("Set wiper position "); Serial.println(wiperPosition);
+  ~Serial.print("Set wiper position "); Serial.println(wiperPosition);
   saveConfigVolume();     // non-volatile storage
 }
 void changeVolume(int diff) {
@@ -196,30 +199,26 @@ void startVolumeScreen() {
   int sz = 20;
   int xx = volButtons[0].x + volButtons[0].w/2; // centerline is halfway in the middle
   int yy = volButtons[0].y + volButtons[0].h/2; // baseline is halfway in the middle
-  //                x0,y0,     x1,y1,     x2,y2,   color
-  tft.fillTriangle(xx-sz,yy,  xx+sz,yy,  xx,yy-sz, cVALUE);  // arrow UP
+  //                  x0,y0,     x1,y1,     x2,y2,   color
+  tft.fillTriangle(xx-sz,yy+4,  xx+sz,yy+4,  xx,yy-sz, cVALUE);  // arrow UP
 
   yy = volButtons[1].y + volButtons[1].h/2;
-  tft.fillTriangle(xx-sz,yy,  xx+sz,yy,  xx,yy+sz, cVALUE);  // arrow DOWN
+  tft.fillTriangle(xx-sz,yy-4,  xx+sz,yy-4,  xx,yy+sz, cVALUE);  // arrow DOWN
 
   gPrevVolIndex = -1;
   updateVolumeScreen();             // fill in values immediately, don't wait for loop() to eventually get around to it
 
   // ----- label this view in upper left corner
-  //showNameOfView("Volume:", cWARN, cBACKGROUND);
-  //delay(4000);                    // no delay - the controller handles the schedule
-  //tft.fillScreen(cBACKGROUND);    // no clear - this screen is visible until the next view clears it
+  //showNameOfView("Volume:", cWARN, cBACKGROUND);  // todo - unused, delete this everywhere
 }
 
 void updateVolumeScreen() {
   // ----- volume
   if (gVolIndex != gPrevVolIndex) {
     initFontSizeBig();
-    erasePrintProportionalText(labelX+16, yRow3, 116, String(gVolIndex), cVALUE);
-  
-    tft.setTextColor(cLABEL);
-    initFontSizeSmall();
-    tft.print(" of 10");
+    //                                 x,y       w  String             color
+    erasePrintProportionalText(labelX+16,yRow2, 36, String(gVolIndex), cVALUE);
+
     gPrevVolIndex = gVolIndex;
   }
 }
@@ -232,7 +231,11 @@ bool onTouchVolume(Point touch) {
      && touch.y >= item.y && touch.y <= item.y+item.h) {
         handled = true;             // hit!
         item.function();            // do the thing
+
+        tft.setCursor(touch.x, touch.y);  // debug: show where hit
+        initFontSizeSmall();
+        tft.print("x");
      }
   }
-  return handled;                   // true=handled, false=controller uses default action
+  return true;  // handled; // debug ALWAYS handle                  // true=handled, false=controller uses default action
 }
