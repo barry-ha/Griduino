97b89f0954c90470da6526c689a992fc4b77e504
barry-ha <barry@electromagneticsoftware.com>
2020-05-17 07:41:13 -0700

    updated to match improvements made to examples\GMT_clock

--- TextField.cpp
+++ TextField.cpp
@@ -14,26 +14,35 @@ uint16_t TextField::cBackground;    // background color
 
 void TextField::eraseOld() {
   // find the height of erasure
-  int16_t x1, y1;
-  uint16_t w, h;
-  tft.getTextBounds(prevText, x, y, &x1, &y1, &w, &h);
+  //int16_t x1, y1;
+  //uint16_t w, h;
+  //tft.getTextBounds(textPrev, x, y, &x1, &y1, &w, &h);
 
-  if (align == FLUSHRIGHT) {
-    x1 -= w;          // move erasure by width of text
-  }
+  //if (align == FLUSHRIGHT) {
+  //  x1 -= w;          // move erasure by width of text
+  //}
 
-  tft.fillRect(x1-2, y1-2, w+4, h+4, cBackground); // erase the requested width of old text
-  //tft.drawRect(x1-2, y1-2, w+4, h+4, ILI9341_RED); // debug: show what area was erased
+  tft.fillRect(xPrev, yPrev, wPrev, hPrev, cBackground); // erase the requested width of old text
+  //tft.drawRect(xPrev-2, yPrev-2, wPrev+4, hPrev+4, ILI9341_RED); // debug: show what area was erased
 }
 void TextField::printNew(const char* pText) {
+  int16_t x1, y1;
+  uint16_t w, h;
+
   int leftedge = x;
-  if (align == FLUSHRIGHT) {
-    int16_t x1, y1;
-    uint16_t w, h;
-    tft.getTextBounds(pText, x, y, &x1, &y1, &w, &h);
-    leftedge -= w;    // move text origin by width of text
+  if (leftedge == -1) {
+    // centered text left-right
+    tft.getTextBounds(pText, 0, y, &x1, &y1, &w, &h);
+    leftedge = (tft.width() - w)/2;
+  }
+  else if (align == FLUSHRIGHT) {
+    tft.getTextBounds(pText, 0, y, &x1, &y1, &w, &h);
+    leftedge = x - w;    // move text origin by width of text
   }
   tft.setCursor(leftedge, y);
   tft.setTextColor(color);
   tft.print(pText);
+
+  // remember region so it can be erased next time
+  tft.getTextBounds(pText, leftedge, y, &xPrev, &yPrev, &wPrev, &hPrev);
 }
--- TextField.h
+++ TextField.h
@@ -14,6 +14,8 @@ class TextField {
   //      Force       txtItem.setDirty();
   //      Print       txtItem.print();
   //
+  // To center text left-right, specify x = -1
+  //
   // Note about proportional fonts:
   // 1. Text origin is bottom left corner
   // 2. Rect origin is upper left corner
@@ -21,15 +23,25 @@ class TextField {
 
   private:
     static uint16_t cBackground; // background color
+    int16_t xPrev, yPrev;   // remember previous text area for next erasure
+    uint16_t wPrev, hPrev;
+    char textPrev[27];      // old text to be erased
 
   public:
-    char prevText[27];      // old text to be erased
     char text[27];          // new text to draw
     int x, y;               // screen coordinates
     uint16_t color;         // text color
     int align;              // FLUSHLEFT | FLUSHRIGHT
     bool dirty;             // true=force reprint even if old=new
 
+    void dump() {
+      // dump the state of this object to the console
+      char buf[128];
+      snprintf(buf, sizeof(buf), "TextField('%s') x,y(%d,%d)", text, x, y);
+      Serial.print(buf);
+      snprintf(buf, sizeof(buf), " Erase x,y,w,h(%d,%d, %d,%d)", xPrev,yPrev, wPrev, hPrev);
+      Serial.println(buf);
+    }
     // ctor - dynamic text field
     TextField(int vxx, int vyy, uint16_t vcc, int valign=FLUSHLEFT) {
       init("", vxx, vyy, vcc, valign);
@@ -46,36 +58,40 @@ class TextField {
     }
     // delegating ctor for common setup code
     void init(const char vtxt[26], int vxx, int vyy, uint16_t vcc, int valign) {
-      strncpy(prevText, vtxt, sizeof(prevText)-1);
+      strncpy(textPrev, vtxt, sizeof(textPrev)-1);
       strncpy(text, vtxt, sizeof(text)-1);
       x = vxx;
       y = vyy;
       color = vcc;
       align = valign;
       dirty = true;
+      xPrev = yPrev = wPrev = hPrev = 0;
     }
 
+    void print(const char* pText) { // dynamic text
+      // main central print routine
+      if (dirty || strcmp(textPrev, pText)) {
+        eraseOld();
+        printNew(pText);
+        strncpy(textPrev, pText, sizeof(textPrev));
+        dirty = false;
+      }
+    }
     void print() {                  // static text
-      printNew(prevText);
-      dirty = false;
-     }
+      // delegate to this->print(char*)
+      print(text);
+    }
     void print(const int d) {       // dynamic integer
+      // format integer and delegate to this->print(char*)
       char sInteger[8];
       snprintf(sInteger, sizeof(sInteger), "%d", d);
       print(sInteger);
     }
-    void print(const char* pText) { // dynamic text
-      if (dirty || strcmp(prevText, pText)) {
-        eraseOld();
-        printNew(pText);
-        strncpy(prevText, pText, sizeof(prevText));
-        dirty = false;
-      }
-    }
     void print(const String str) {  // dynamic String
+      // format String object and delegate to this->print(char*)
       char temp[ str.length()+1 ];
       str.toCharArray(temp, sizeof(temp));
-      print(temp);
+      print(temp); // delegate to this->print(char*)
     }
     static void setTextDirty(TextField* pTable, int count) {
       // Mark all text fields "dirty" to force reprinting them at next usage
@@ -84,6 +100,7 @@ class TextField {
       }
     }
     void setBackground(uint16_t bkg) {
+      // Set all text fields background color
       cBackground = bkg;
     }
 
