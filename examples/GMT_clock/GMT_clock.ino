e50db875f95bc7aae2d819dacb8bddc6a730f7d3
barry-ha <barry@electromagneticsoftware.com>
2020-05-21 06:27:17 -0700

    indentation (no functional change)
    Make the white space exactly match the primary constants.h file. Since I use 'Beyond Compare' to keep examples in sync with the Griduino program, this helps reduce the clutter during comparisons.

--- examples/GMT_clock/GMT_clock.ino
+++ examples/GMT_clock/GMT_clock.ino
@@ -147,18 +147,18 @@ int gSatellites = 0;                    // number of satellites
 
 // ----- color scheme
 // RGB 565 color code: http://www.barth-dev.de/online/rgb565-color-picker/
-#define cBACKGROUND      0x00A             // 0,   0,  10 = darker than ILI9341_NAVY, but not black
-//efine cSCALECOLOR      0xF844
-#define cTEXTCOLOR       ILI9341_CYAN      // 0, 255, 255
-#define cTEXTFAINT       0x514             // 0, 160, 160 = blue, between CYAN and DARKCYAN
-#define cLABEL           ILI9341_GREEN
-#define cVALUE           ILI9341_YELLOW
-#define cINPUT           ILI9341_WHITE
-//efine cHIGHLIGHT       ILI9341_WHITE
-#define cBUTTONFILL      ILI9341_NAVY
-#define cBUTTONOUTLINE   ILI9341_BLUE      // 0,   0, 255 = darker than cyan
-#define cBUTTONLABEL     ILI9341_YELLOW
-#define cWARN            0xF844            // brighter than ILI9341_RED but not pink
+#define cBACKGROUND     0x00A             // 0,   0,  10 = darker than ILI9341_NAVY, but not black
+//efine cSCALECOLOR     0xF844
+#define cTEXTCOLOR      ILI9341_CYAN      // 0, 255, 255
+#define cTEXTFAINT      0x514             // 0, 160, 160 = blue, between CYAN and DARKCYAN
+#define cLABEL          ILI9341_GREEN
+#define cVALUE          ILI9341_YELLOW
+#define cINPUT          ILI9341_WHITE
+//efine cHIGHLIGHT      ILI9341_WHITE
+#define cBUTTONFILL     ILI9341_NAVY
+#define cBUTTONOUTLINE  ILI9341_BLUE      // 0,   0, 255 = darker than cyan
+#define cBUTTONLABEL    ILI9341_YELLOW
+#define cWARN           0xF844            // brighter than ILI9341_RED but not pink
 
 // ========== constants ===============================
 
