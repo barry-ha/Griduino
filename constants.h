9e73ca7e5d225c27792504230d8c8eafaefd5684
barry-ha <barry@electromagneticsoftware.com>
2020-07-05 10:34:54 -0700

    v0.17 for "settings" view

--- constants.h
+++ constants.h
@@ -3,7 +3,7 @@
 
 // ------- Identity for splash screen and console --------
 #define PROGRAM_TITLE   "Griduino"
-#define PROGRAM_VERSION "v0.16"
+#define PROGRAM_VERSION "v0.17"
 #define PROGRAM_LINE1   "Barry K7BWH"
 #define PROGRAM_LINE2   "John KM7O"
 #define PROGRAM_COMPILED __DATE__ " " __TIME__
