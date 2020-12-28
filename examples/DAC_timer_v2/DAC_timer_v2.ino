977db3c2d8016a551f4b62258ce63eec7cf10793
barry-ha <barry@electromagneticsoftware.com>
2020-12-27 19:08:41 -0800

    added test program that generates audio tones by playing a waveform table from interrupt level

---
+++ examples/DAC_timer_v2/DAC_timer_v2.ino
@@ -0,0 +1,193 @@
+/*
+  DAC Timer v2 -- basic audio tone using interrupt level to control DAC
+
+  Change log:
+            2020-12-24 created
+
+  Software: Barry Hansen, K7BWH, barry@k7bwh.com, Seattle, WA
+  Hardware: John Vanderbeck, KM7O, Seattle, WA
+
+  Purpose:  Generate an audio sine wave in the speaker from an ISR.
+            Provide an interface to change the tone's frequency.
+            This is another small step toward playing WAV files with 8 kHz encoding.
+
+  Guess'n check table size:
+            * 800 Hz = each complete cycle takes 1/800 = 1,250 usec
+            * 20 steps/wave = 1250/20 = 63 usec per interrupt = 16 kHz
+
+            But I want an 8 kHz interrupt rate (not 16 kHz) to match the 
+            sample rate of WAV files. So:
+            * 800 Hz @ 10 steps/wave = 1/800/10 = 125 usec per interrupt
+            * Resulting in a waveform lookup table 10 steps/wave
+
+            Note the lookup table size varies for the desired audio frequency.
+            If you want to generate, say, 1100 Hz audio tone:
+            * 1100 Hz = each complete cycle takes 1/1100 = 909 usec
+            * For 1600 Hz: table size = (8000 Hz)/(1600 Hz) =  5.00 entries
+            * For 1200 Hz: table size = (8000 Hz)/(1200 Hz) =  6.67 entries
+            * For 1100 Hz: table size = (8000 Hz)/(1100 Hz) =  7.27 entries
+            * For 1000 Hz: table size = (8000 Hz)/(1000 Hz) =  8.00 entries
+            * For  800 Hz: table size = (8000 Hz)/( 800 Hz) = 10.00 entries 
+            * For  700 Hz: table size = (8000 Hz)/( 700 Hz) = 11.43 entries
+            * For  400 Hz: table size = (8000 Hz)/( 400 Hz) = 20.00 entries
+
+  Note:     Does not control the backlight.
+            Does not use the TFT display at all.
+            Remove the TFT display and attach an oscilloscope to the test points.
+            Open serial console for status messages.
+
+  Conclusion: 
+            For a fixed interrupt rate of 8 kHz, the small number of audio 
+            frequencies available (12) is unacceptable for Morse code sidetone. 
+            Do not use a fixed 8 kHz interrupt for self-generated waveforms.
+
+            For a fixed number of samples/waveform (variable interrupt rate), 
+            the best audio quality should have 10 steps per sine wave.
+            Processor seems to run fine at higher frequencies, tested up to 
+            1,600 Hz audio = 16 kHz interrupt rate.
+
+  Inspired by:
+            Mark Fickett, KB3JCY, morse package.
+            https://github.com/markfickett/arduinomorse
+  Requires: 
+            Dennis-van-Gils/SAMD51_InterruptTimer
+            https://github.com/Dennis-van-Gils/SAMD51_InterruptTimer 
+*/
+
+#include <Arduino.h>
+#include <Adafruit_ILI9341.h>         // TFT color display library
+#include <DS1804.h>                   // DS1804 digital potentiometer library
+#include "morse_isr.h"                // Morse Code using digital-audio converter DAC0
+
+// ------- Identity for splash screen and console --------
+#define PROGRAM_TITLE   "DAC Timer v2"
+#define PROGRAM_VERSION "v0.30"
+#define PROGRAM_LINE1   "Barry K7BWH"
+#define PROGRAM_LINE2   "John KM7O"
+#define PROGRAM_COMPILED __DATE__ " " __TIME__
+
+// ------------ Audio output
+#define DAC_PIN      DAC0             // onboard DAC0 == pin A0
+#define PIN_SPEAKER  DAC0             // uses DAC
+
+// Adafruit Feather M4 Express pin definitions
+#define PIN_VCS      A1               // volume chip select
+#define PIN_VINC      6               // volume increment
+#define PIN_VUD      A2               // volume up/down
+
+// ctor         DS1804( ChipSel pin, Incr pin,  U/D pin,  maxResistance (K) )
+DS1804 volume = DS1804( PIN_VCS,     PIN_VINC,  PIN_VUD,  DS1804_TEN );
+int gWiper = 15;                      // initial digital potentiometer wiper position, 0..99
+
+// ------------ definitions
+const int howLongToWait = 4;          // max number of seconds at startup waiting for Serial port to console
+
+// ----- console Serial port helper
+void waitForSerial(int howLong) {
+  // Adafruit Feather M4 Express takes awhile to restore its USB connx to the PC
+  // and the operator takes awhile to restart the console (Tools > Serial Monitor)
+  // so give them a few seconds for this to settle before sending messages to IDE
+  unsigned long targetTime = millis() + howLong*1000;
+  while (millis() < targetTime) {
+    if (Serial) break;
+  }
+}
+
+//=========== setup ============================================
+void setup() {
+
+  // ----- init TFT backlight
+  //pinMode(TFT_BL, OUTPUT);          // <------ THIS CAUSES PROGRAM CRASH 
+  //analogWrite(TFT_BL, 255);         // <------ THIS CAUSES PROGRAM CRASH 
+                                      // Symptom: screen is dark, LED stops flashing, USB port non-responsive
+                                      // To recover, double-click Feather's "Reset" button and load another pgm
+
+  // ----- init serial monitor
+  Serial.begin(115200);               // init for debuggging in the Arduino IDE
+  waitForSerial(howLongToWait);       // wait for developer to connect debugging console
+
+  // now that Serial is ready and connected (or we gave up)...
+  Serial.println(PROGRAM_TITLE " " PROGRAM_VERSION);  // Report our program name to console
+  Serial.println("Compiled " PROGRAM_COMPILED);       // Report our compiled date
+  Serial.println(__FILE__);                           // Report our source code file name
+  Serial.println("CPU Frequency = " + String(F_CPU / 1000000) + " MHz");
+
+  // ----- init digital potentiometer
+  volume.unlock();                    // unlock digipot (in case someone else, like an example pgm, has locked it)
+  volume.setToZero();                 // set digipot hardware to match its ctor (wiper=0) because the chip cannot be read
+                                      // and all "setWiper" commands are really incr/decr pulses. This gets it sync.
+  volume.setWiperPosition(20);        // set default volume in digital pot, '20' is reasonable
+
+  // ----- init DAC for audio/morse code
+  #if defined(SAMD_SERIES)
+    // Only set DAC resolution on devices that have a DAC
+    analogWriteResolution(12);        // 1..32, sets DAC output resolution to 12 bit (4096 levels)
+                                      // because Feather M4 maximum output resolution is 12 bit
+  #endif
+
+  // ---------- Morse Code ----------
+  int gFrequency = 800;               // initial Morse code sidetone pitch
+  int gWordsPerMinute = 15;           // initial Morse code sending speed
+
+  // ---------- Interrupt handler ---
+  int nPause = 2000;                  // pause to listen to each tone, msec
+
+  // --------------------------------------------------------------------------------- //
+  //           Test range of tones at constant 8 KHz rate                              //
+  // --------------------------------------------------------------------------------- //
+  // Conclusions:
+  //      The small choice of audio frequencies available is unacceptable
+  //      for Morse code sidetone. Do not use a fixed 8 kHz interrupt for
+  //      self-generated waveforms.
+
+  Serial.println("\n***** Testing Fixed 8 kHz Interrupt Rate (variable number samples/waveform *****\n");
+
+  for (int kk=16; kk>=3; kk--)
+  {
+    const unsigned int isrFrequency = 8000;  // 8 kHz interrupt rate to play WAV files
+    DACMorseSenderISR dacTone(DAC_PIN, isrFrequency);
+
+    unsigned int audioFreq = 8000 / kk;
+
+    Serial.print("----- Tone "); Serial.print(kk); Serial.print(". "); Serial.print(audioFreq); Serial.println(" Hz -----");
+    dacTone.setup(audioFreq, gWordsPerMinute);
+    //dacTone.dump();
+    dacTone.start_tone();
+    delay(nPause);
+    dacTone.stop_tone();
+    delay(25);
+  }
+
+  // --------------------------------------------------------------------------------- //
+  //           Test range of tones at constant N samples/waveform                      //
+  // --------------------------------------------------------------------------------- //
+  // Conclusions:
+  //       4 samples/wave is too crude and sounds very raspy
+  //       6 samples/wave is crude, sounds raspy, has an overtone below 650 Hz
+  //       8 samples/wave is better, has an overtone below 480 Hz
+  //      10 samples/wave sounds quite nice, has no overtone
+  //      This speaker does not respond <400 Hz no matter how smooth the waveform.
+
+  Serial.println("\n***** Testing Fixed Number of Samples/Waveform (variable interrupt rate) *****\n");
+
+  for (int ii=0; ii<3; ii++) {
+    Serial.print("======================"); Serial.println(ii);
+    nPause = 750;
+    for (unsigned int audioFreq=400; audioFreq<=800; audioFreq=audioFreq*51/50) {
+      int interruptFreq = 4 * audioFreq;
+      Serial.print("----- Tone "); Serial.print(audioFreq); Serial.println(" Hz -----");
+      DACMorseSenderISR dacTone(DAC_PIN, interruptFreq);
+      dacTone.setup(audioFreq, gWordsPerMinute);
+      dacTone.start_tone();
+      delay(nPause);
+      dacTone.stop_tone();
+    }
+    delay(2000);                  // a short break between series
+    
+  }
+}
+
+//=========== main work loop ===================================
+void loop() {
+
+}
---
+++ examples/DAC_timer_v2/morse_isr.h
@@ -0,0 +1,97 @@
+#pragma once
+
+/**
+ * Send Morse Code with a sine wave using onboard DAC to a speaker.
+ * This version sends in a blocking manner (does not return until message complete).
+ *
+ * All input should be lowercase.
+ * Prosigns (SK, KN, etc) have special character values #defined.
+ *
+ * This was inspired by the fine example by Mark Fickett, KB3JCY, morse package.
+ *        https://github.com/markfickett/arduinomorse
+ */
+
+// Punctuation and Prosigns
+#define PROSIGN_SK  'S'               // end of transmission
+#define PROSIGN_KN  'K'               // slash
+#define PROSIGN_BT  'B'               // dash
+#define PROSIGN_AS  'A'               // wait
+#define PROSIGN_IMI 'Q'               // question
+#define PROSIGN_MIM 'C'               // comma
+#define PROSIGN_AAA '.'               // period
+
+class DACMorseSenderISR {
+  private:
+    String message;
+
+    // DAC settings
+    int dacPin;                       // identifies DAC output port
+    int isrFreq;                      // interrupt service request rate, Hz
+    float dacSampleTime;              // time to hold each waveform sample, microseconds
+    unsigned int nSamples;            // number of entries in waveform lookup table
+    unsigned int* pSamples;           // ptr to waveform lookup table
+
+    float audioFreq = 800;           // audio frequency, Hz (default 800 Hz)
+    const int dacAmplitude = 2040;    // = slightly less than half of 2^12, to prevent overflow from rounding errors
+    const int dacOffset = 2048;       // = exactly half of 2^12
+                                      // Requirement is: dacOffset +/- dacAmplitude = voltage range of DAC
+    // Morse settings
+    float wpm;                        // words per minute
+
+    float fDitDuration;               // seconds per dit
+    int   iDitDuration;               // msec    per dit
+    float letterSpace;                // typ 3x ditDuration (seconds)
+    float wordSpace;                  // typ 7x ditDuration (seconds)
+
+    int cyclesPerDit;                 // number of waveforms to fill an entire DIT
+    int cyclesPerDah;                 // number of waveforms to fill an entire DAH
+
+  public:
+	/**
+	 * Create a sender for the given DAC port, audio frequency, and sending rate.
+   * isrFreq = 8000 Hz to play WAV files
+	 */
+    DACMorseSenderISR(int outputPin, unsigned int isrFrequency) {
+      dacPin = outputPin;
+      isrFreq = isrFrequency;
+      dacSampleTime = 1E6 / isrFreq;  // usec between interrupts
+      nSamples = isrFreq / audioFreq;// number of waveform table entries needed
+      pSamples = (unsigned int*) malloc(nSamples * sizeof(unsigned int));
+    }
+
+    ~DACMorseSenderISR() {
+      free(pSamples);
+    }
+  
+    /**
+     * To be called during the Arduino setup().
+     * Create the waveform lookup table.
+     * iFreq = min(100 Hz), max(1200 Hz), if you exceed this the output is undefined 
+     */
+    void setup(int iFreq, float fWPM);
+
+    /**
+     * Set the message to be sent.
+     */
+    void setMessage(const String newMessage);
+
+    /**
+     * Send the entirety of the current message before returning.
+     */
+    void sendBlocking();
+
+    /**
+     * Deprecated: use these for unit test
+     */
+    void dump();
+    void start_tone();                  // asynchronous ISR, non-blocking
+    void stop_tone();                   // asynchronous ISR, non-blocking
+    void send_dit();
+    void send_dah();
+    void send_dit_space();
+    void send_letter_space();
+    void send_word_space();
+
+  private:
+    void send(char c);
+};
