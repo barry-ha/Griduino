26f6305e11167881377057bbb5aa74101daccec1
barry-ha <barry@electromagneticsoftware.com>
2021-01-30 06:58:38 -0800

    starting work on sending Morse code from interrupt level, an unfinished prototype

---
+++ examples/DAC_Morse_v3/morse_dac.h
@@ -0,0 +1,80 @@
+#pragma once
+/**
+ * Send Morse Code with a sine wave using onboard DAC to a speaker.
+ * This version sends in a blocking manner (does not return until message complete).
+ *
+ * All input should be lowercase.
+ * Prosigns (SK, KN, etc) have special character values #defined.
+ *
+ * This was inspired by the fine example by Mark Fickett, KB3JCY, morse package.
+ *        https://github.com/markfickett/arduinomorse
+ */
+
+// Punctuation and Prosigns
+#define PROSIGN_SK  'S'
+#define PROSIGN_KN  'K'
+#define PROSIGN_BT  'B'
+#define PROSIGN_AS  'A'
+
+class DACMorseSender {
+  private:
+    String message;
+
+    // DAC settings
+    int dacPin;           // identifies DAC output port
+    float fFrequency;     // Hz
+    float dacSampleTime;  // microseconds to hold each waveform sample
+
+    const int dacAmplitude = 2040;  // = slightly less than half of 2^12, to prevent overflow from rounding errors
+    const int dacOffset = 2048;     // = exactly half of 2^12
+                                    // Requirement is: dacOffset +/- dacAmplitude = voltage range of DAC
+    // Morse settings
+    float wpm;            // words per minute
+
+    float fDitDuration;   // seconds per dit
+    int   iDitDuration;   // msec    per dit
+    float letterSpace;    // = ditDuration * 10 (seconds)
+    float wordSpace;      // = ditDuration * 24 (seconds)
+
+    int cyclesPerDit;     // number of waveforms to fill an entire DIT
+    int cyclesPerDah;     // number of waveforms to fill an entire DAH
+
+  public:
+	/**
+	 * Create a sender for the given DAC port, audio frequency, and sending rate.
+	 */
+    DACMorseSender(int outputPin, int iFreq, float fWPM) {
+      dacPin = outputPin;
+      fFrequency = iFreq;
+      wpm = fWPM;
+    }
+  
+    /**
+     * To be called during the Arduino setup().
+     * Create the waveform lookup table.
+     */
+    void setup();
+
+    /**
+     * Set the message to be sent.
+     */
+    void setMessage(const String newMessage);
+
+    /**
+     * Send the entirety of the current message before returning.
+     */
+    void sendBlocking();
+
+    /**
+     * Deprecated: use these for unit test
+     */
+    void dump();
+    void send_dit();
+    void send_dah();
+    void send_dit_space();
+    void send_letter_space();
+    void send_word_space();
+
+  private:
+    void send(char c);
+};
