04a7707a63e28036375dbfec37775bfa1df9e907
barry-ha <barry@electromagneticsoftware.com>
2022-11-12 08:20:19 -0800

    formatted source with clang

--- examples/Reset/Reset.ino
+++ examples/Reset/Reset.ino
@@ -7,9 +7,9 @@
 
   Source:
   - Original example blink code is in the public domain.
-  - good: https://forum.arduino.cc/t/soft-reset-and-arduino/367284/5 
+  - good: https://forum.arduino.cc/t/soft-reset-and-arduino/367284/5
   - fail: https://www.instructables.com/two-ways-to-reset-arduino-in-software/
-  - fail: https://www.theengineeringprojects.com/2015/11/reset-arduino-programmatically.html 
+  - fail: https://www.theengineeringprojects.com/2015/11/reset-arduino-programmatically.html
  */
 
 // Pin 13 has an LED connected on most Arduino boards, including Feather M4.
@@ -18,8 +18,7 @@
 // Pin 13 has the LED on Teensy 3.0
 int led = 13;
 
-
-// the setup routine runs once when you press reset:
+// the setup routine runs once when you press reset
 void setup() {
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT);
@@ -52,14 +51,15 @@ void loop() {
 
 // ----- success: both Feather M4 and RP2040 will reset as desired
 // use library manager to install Adafruit_SleepyDog
-#include <Adafruit_SleepyDog.h>  // https://github.com/adafruit/Adafruit_SleepyDog
+#include <Adafruit_SleepyDog.h>   // https://github.com/adafruit/Adafruit_SleepyDog
 void reboot_wdt() {
-  Watchdog.enable(15);  // milliseconds timeout
-  while (1) {}
+  Watchdog.enable(15);   // milliseconds timeout
+  while (1) {
+  }
 }
 
 // ----- fail: the Feather M4 does not reconnect to USB
 void reboot_function() {
-  void (*resetFunction)(void) = 0;  //declare reset function at address 0
-  resetFunction();                  // does not return
+  void (*resetFunction)(void) = 0;   // declare reset function at address 0
+  resetFunction();                   // does not return
 }
