3992cc4c6ff5cd8ed3e30c0c4bbd67e665019573
barry-ha <barry@electromagneticsoftware.com>
2022-11-19 10:15:31 -0800

    removed #ifdef ARDUINO_AVR_MEGA2560
    We're never going back to small old Arduinos

--- hardware.h
+++ hardware.h
@@ -67,14 +67,6 @@ On-board lights:
 #define SD_CD  10   // SD card detect pin - Feather
 #define SD_CCS 11   // SD card select pin - Feather
 
-#elif defined(ARDUINO_AVR_MEGA2560)
-#define TFT_BL 6    // TFT backlight
-#define SD_CCS 7    // SD card select pin - Mega
-#define SD_CD  8    // SD card detect pin - Mega
-#define TFT_DC 9    // TFT display/command pin
-#define TFT_CS 10   // TFT chip select pin
-#define BMP_CS 13   // BMP388 sensor, chip select
-
 #else
 #warning You need to define pins for your SPI bus hardware
 #endif
