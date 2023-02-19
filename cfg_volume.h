9fe01a2d6c2b4f44da203ed326dcf3dfc61ec2fd
John KM7O <jcvanderbeck@gmail.com>
2023-02-19 14:29:23 -0800

    commented out using A1

--- cfg_volume.h
+++ cfg_volume.h
@@ -134,10 +134,10 @@ protected:
     // @param volIndex = 0..10
     int wiperPosition = volLevel[volIndex];
 
-#define PIN_VCS A1              // chip select pin for DS1804 volume control
-    pinMode(PIN_VCS, OUTPUT);   // fix bug that somehow forgets this is an output pin
-    volume.unlock();            // enable (set low)
-    volume.setWiperPosition(wiperPosition);
+//#define PIN_VCS A1              // chip select pin for DS1804 volume control
+//    pinMode(PIN_VCS, OUTPUT);   // fix bug that somehow forgets this is an output pin
+//    volume.unlock();            // enable (set low)
+//    volume.setWiperPosition(wiperPosition);
 
     char msg[256];
     snprintf(msg, 256, "Set volume index %d, wiper position %d", volIndex, wiperPosition);   // debug
