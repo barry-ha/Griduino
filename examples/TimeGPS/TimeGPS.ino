4d5a2613bea7aba001d8f5d185864868f4876f1f
barry-ha <barry@electromagneticsoftware.com>
2023-04-11 03:31:46 -0700

    formatted source code with clang
    No functional changes.

--- examples/TimeGPS/TimeGPS.ino
+++ examples/TimeGPS/TimeGPS.ino
@@ -1,8 +1,8 @@
 /*
   TimeGPS -- demonstrate reading time-of-day from GPS
              and usage of TimeLib for date operations.
-             This will become the basis for graphing 
-             barometric pressure, which relies heavily on 
+             This will become the basis for graphing
+             barometric pressure, which relies heavily on
              date calculations.
 
   Date:     2019-08-30 created from example BorisNeubert / Time
@@ -12,8 +12,8 @@
 
 */
 
-#include <Adafruit_GPS.h>             // Ultimate GPS library
-#include "constants.h"                // Griduino constants, colors, typedefs
+#include <Adafruit_GPS.h>   // Ultimate GPS library
+#include "constants.h"      // Griduino constants, colors, typedefs
 #include <TimeLib.h>
 
 // ------- Identity for splash screen and console --------
@@ -26,14 +26,14 @@
 Adafruit_GPS GPS(&Serial1);
 
 // ------------ definitions
-const int howLongToWait = 8;  // max number of seconds at startup waiting for Serial port to console
+const int howLongToWait = 8;   // max number of seconds at startup waiting for Serial port to console
 
 // Offset hours from gps time (UTC)
-//const int offset = 1;   // Central European Time
-//const int offset = -5;  // Eastern Standard Time (USA)
-//const int offset = -4;  // Eastern Daylight Time (USA)
-//const int offset = -8;  // Pacific Standard Time (USA)
-const int offset = -7;  // Pacific Daylight Time (USA)
+// const int offset = 1;   // Central European Time
+// const int offset = -5;  // Eastern Standard Time (USA)
+// const int offset = -4;  // Eastern Daylight Time (USA)
+// const int offset = -8;  // Pacific Standard Time (USA)
+const int offset = -7;   // Pacific Daylight Time (USA)
 
 // Ideally, it should be possible to learn the time zone
 // based on the GPS position data.  However, that would
@@ -41,7 +41,7 @@ const int offset = -7;  // Pacific Daylight Time (USA)
 // sort of database using Eric Muller's time zone shape
 // maps, at http://efele.net/maps/tz/
 
-time_t prevDisplay = 0; // when the digital clock was displayed
+time_t prevDisplay = 0;   // when the digital clock was displayed
 
 //=========== time helpers =================================
 // Did the GPS report a valid date?
@@ -67,11 +67,14 @@ void printDigits(int digits) {
   Serial.print(digits);
 }
 
-void digitalClockDisplay(){
+void digitalClockDisplay() {
   // digital clock display of the time
-  Serial.print(month());  Serial.print("-");
-  Serial.print(day());    Serial.print("-");
-  Serial.print(year());   Serial.print("  "); 
+  Serial.print(month());
+  Serial.print("-");
+  Serial.print(day());
+  Serial.print("-");
+  Serial.print(year());
+  Serial.print("  ");
 
   Serial.print(hour());
   printDigits(minute());
@@ -84,9 +87,10 @@ void waitForSerial(int howLong) {
   // Adafruit Feather M4 Express takes awhile to restore its USB connx to the PC
   // and the operator takes awhile to restart the console (Tools > Serial Monitor)
   // so give them a few seconds for this to settle before sending messages to IDE
-  unsigned long targetTime = millis() + howLong*1000;
+  unsigned long targetTime = millis() + howLong * 1000;
   while (millis() < targetTime) {
-    if (Serial) break;
+    if (Serial)
+      break;
   }
 }
 
@@ -94,24 +98,24 @@ void waitForSerial(int howLong) {
 void setup() {
 
   // ----- init serial monitor
-  Serial.begin(115200);               // init for debuggging in the Arduino IDE
-  waitForSerial(howLongToWait);       // wait for developer to connect debugging console
+  Serial.begin(115200);           // init for debuggging in the Arduino IDE
+  waitForSerial(howLongToWait);   // wait for developer to connect debugging console
 
   // now that Serial is ready and connected (or we gave up)...
-  Serial.println(TIMEGPS_TITLE " " PROGRAM_VERSION);  // Report our program name to console
-  Serial.println("Compiled " PROGRAM_COMPILED);       // Report our compiled date
-  Serial.println(__FILE__);                           // Report our source code file name
+  Serial.println(TIMEGPS_TITLE " " PROGRAM_VERSION);   // Report our program name to console
+  Serial.println("Compiled " PROGRAM_COMPILED);        // Report our compiled date
+  Serial.println(__FILE__);                            // Report our source code file name
 
   // ----- init GPS
-  GPS.begin(9600);                              // 9600 NMEA is the default baud rate for Adafruit MTK GPS's
-  delay(200);                                   // is delay really needed?
-  GPS.sendCommand(PMTK_SET_BAUD_57600);         // set baud rate to 57600
+  GPS.begin(9600);                        // 9600 NMEA is the default baud rate for Adafruit MTK GPS's
+  delay(200);                             // is delay really needed?
+  GPS.sendCommand(PMTK_SET_BAUD_57600);   // set baud rate to 57600
   delay(200);
   GPS.begin(57600);
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); // turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);   // turn on RMC (recommended minimum) and GGA (fix data) including altitude
 
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);    // 1 Hz update rate
-  //GPS.sendCommand(PMTK_SET_NMEA_UPDATE_200_MILLIHERTZ); // Once every 5 seconds update
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  // GPS.sendCommand(PMTK_SET_NMEA_UPDATE_200_MILLIHERTZ); // Once every 5 seconds update
 
   Serial.println("Waiting for GPS time ... ");
 }
@@ -125,17 +129,17 @@ void loop() {
     // sentence received -- verify checksum, parse it
     // GPS parsing: https://learn.adafruit.com/adafruit-ultimate-gps/parsed-data-output
     // In this "TimeGPS" program, all we need is the RTC date and time, which is sent
-    // from the GPS module to the Arduino as NMEA sentences. 
+    // from the GPS module to the Arduino as NMEA sentences.
     if (GPS.parse(GPS.lastNMEA())) {
       // when TinyGPS reports new data...
       unsigned long age;
       int Year;
-      //byte Month, Day, Hour, Minute, Second;
-      //gps.crack_datetime(&Year, &Month, &Day, &Hour, &Minute, &Second, NULL, &age);
+      // byte Month, Day, Hour, Minute, Second;
+      // gps.crack_datetime(&Year, &Month, &Day, &Hour, &Minute, &Second, NULL, &age);
       if (true) /*(age < 500)*/ {
         // set the Time to the latest GPS reading
         setTime(GPS.hour, GPS.minute, GPS.seconds, GPS.day, GPS.month, GPS.year);
-        adjustTime(offset * SECS_PER_HOUR);     // update to our internal selected time zone
+        adjustTime(offset * SECS_PER_HOUR);   // update to our internal selected time zone
       }
     } else {
       // parsing failed -- restart main loop to wait for another sentence
@@ -145,15 +149,17 @@ void loop() {
   }
 
   if (timeStatus() != timeNotSet) {
-    if (now() != prevDisplay) {   // update the display only if the time has changed
-      if (isDateValid(year(), month(), day())) {  // update the display only if the time is valid
-        digitalClockDisplay();  
-  	  } else {
-        Serial.print("Still waiting because date = "); 
-        Serial.print(month()); Serial.print("-");
-        Serial.print(day()); Serial.print("-");
+    if (now() != prevDisplay) {                    // update the display only if the time has changed
+      if (isDateValid(year(), month(), day())) {   // update the display only if the time is valid
+        digitalClockDisplay();
+      } else {
+        Serial.print("Still waiting because date = ");
+        Serial.print(month());
+        Serial.print("-");
+        Serial.print(day());
+        Serial.print("-");
         Serial.println(year());
-	    }
+      }
       prevDisplay = now();
     }
   }
