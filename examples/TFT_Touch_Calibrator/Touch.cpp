01ae236a848c911a504a76b6376ad2cfbbfb9837
barry-ha <barry@electromagneticsoftware.com>
2023-12-24 08:29:07 -0800

    formatted source with clang (no functional changes)

--- examples/TFT_Touch_Calibrator/Touch.cpp
+++ examples/TFT_Touch_Calibrator/Touch.cpp
@@ -5,14 +5,14 @@
   Hardware: John Vanderbeck, KM7O, Seattle, WA
 
  * Purpose: Contains the touchscreen code to get it out of the way
- * 
+ *
  */
 
-#include "Adafruit_GFX.h"           // Core graphics display library
-#include "TouchScreen.h"            // Touchscreen built in to 3.2" Adafruit TFT display
+#include "Adafruit_GFX.h"   // Core graphics display library
+#include "TouchScreen.h"    // Touchscreen built in to 3.2" Adafruit TFT display
 
 // ---------- constants
-#define SCREENWIDTH 320
+#define SCREENWIDTH  320
 #define SCREENHEIGHT 240
 
 // ---------- Touch Screen
@@ -20,19 +20,19 @@
 // between X+ and X- Use any multimeter to read it
 // This sketch has just one touch area that covers the entire screen
 #if defined(SAMD_SERIES)
-  // Adafruit Feather M4 Express pin definitions
-  #define PIN_XP  A3    // Touchscreen X+ can be a digital pin
-  #define PIN_XM  A4    // Touchscreen X- must be an analog pin, use "An" notation
-  #define PIN_YP  A5    // Touchscreen Y+ must be an analog pin, use "An" notation
-  #define PIN_YM   9    // Touchscreen Y- can be a digital pin
+// Adafruit Feather M4 Express pin definitions
+#define PIN_XP A3   // Touchscreen X+ can be a digital pin
+#define PIN_XM A4   // Touchscreen X- must be an analog pin, use "An" notation
+#define PIN_YP A5   // Touchscreen Y+ must be an analog pin, use "An" notation
+#define PIN_YM 9    // Touchscreen Y- can be a digital pin
 #elif defined(ARDUINO_AVR_MEGA2560)
-  // Arduino Mega 2560 and others
-  #define PIN_XP   4    // Touchscreen X+ can be a digital pin
-  #define PIN_XM  A3    // Touchscreen X- must be an analog pin, use "An" notation
-  #define PIN_YP  A2    // Touchscreen Y+ must be an analog pin, use "An" notation
-  #define PIN_YM   5    // Touchscreen Y- can be a digital pin
+// Arduino Mega 2560 and others
+#define PIN_XP 4    // Touchscreen X+ can be a digital pin
+#define PIN_XM A3   // Touchscreen X- must be an analog pin, use "An" notation
+#define PIN_YP A2   // Touchscreen Y+ must be an analog pin, use "An" notation
+#define PIN_YM 5    // Touchscreen Y- can be a digital pin
 #else
-  #warning You need to define pins for your hardware
+#warning You need to define pins for your hardware
 
 #endif
 extern TouchScreen ts;
@@ -44,7 +44,7 @@ struct Point {
 
 // ============== touchscreen helpers ==========================
 
-void mapTouchToScreen(TSPoint touch, Point* screen, int orientation) {
+void mapTouchToScreen(TSPoint touch, Point *screen, int orientation) {
   // convert from X+,Y+ resistance measurements to screen coordinates
   // param touch = resistance readings from touchscreen
   // param screen = result of converting touch into screen coordinates
@@ -61,8 +61,8 @@ void mapTouchToScreen(TSPoint touch, Point* screen, int orientation) {
 
   // setRotation(1) = landscape orientation = x-,y-axis exchanged
   //          map(value    in_min,in_max, out_min,out_max)
-  screen->x = map(touch.y,  150,880,      0, SCREENWIDTH);
-  screen->y = map(touch.x,  860,130,      0, SCREENHEIGHT);
+  screen->x = map(touch.y, 150, 880, 0, SCREENWIDTH);
+  screen->y = map(touch.x, 860, 130, 0, SCREENHEIGHT);
   if (orientation == 3) {
     // if display is flipped, then also flip both x,y touchscreen coords
     screen->x = SCREENWIDTH - screen->x;
@@ -71,13 +71,13 @@ void mapTouchToScreen(TSPoint touch, Point* screen, int orientation) {
   return;
 }
 
-bool gTouching = false;             // keep track of previous state
-bool newScreenTap(Point* pPoint, int orientation) {
+bool gTouching = false;   // keep track of previous state
+bool newScreenTap(Point *pPoint, int orientation) {
   // find leading edge of a screen touch
   // returns TRUE only once on initial screen press
   // if true, also return screen coordinates of the touch
 
-  bool result = false;        // assume no touch
+  bool result = false;   // assume no touch
   if (gTouching) {
     // the touch was previously processed, so ignore continued pressure until they let go
     if (!ts.isTouching()) {
@@ -90,15 +90,18 @@ bool newScreenTap(Point* pPoint, int orientation) {
     // The built-in "isTouching" function does most of the debounce and threshhold detection needed
     if (ts.isTouching()) {
       gTouching = true;
-      result = true;
+      result    = true;
 
       // touchscreen point object has (x,y,z) coordinates, where z = pressure
       TSPoint touch = ts.getPoint();
 
       // convert resistance measurements into screen pixel coords
       mapTouchToScreen(touch, pPoint, orientation);
-      Serial.print("Screen touched at ("); Serial.print(pPoint->x);
-      Serial.print(","); Serial.print(pPoint->y); Serial.println(")");
+      Serial.print("Screen touched at (");
+      Serial.print(pPoint->x);
+      Serial.print(",");
+      Serial.print(pPoint->y);
+      Serial.println(")");
     }
   }
   // delay(10);   // no delay: code above completely handles debouncing without blocking the loop
@@ -110,11 +113,15 @@ bool newScreenTap(Point* pPoint, int orientation) {
 
 // 2020-05-03 CraigV and barry@k7bwh.com
 uint16_t myPressure(void) {
-  pinMode(PIN_XP, OUTPUT);   digitalWrite(PIN_XP, LOW);   // Set X+ to ground
-  pinMode(PIN_YM, OUTPUT);   digitalWrite(PIN_YM, HIGH);  // Set Y- to VCC
+  pinMode(PIN_XP, OUTPUT);
+  digitalWrite(PIN_XP, LOW);   // Set X+ to ground
+  pinMode(PIN_YM, OUTPUT);
+  digitalWrite(PIN_YM, HIGH);   // Set Y- to VCC
 
-  digitalWrite(PIN_XM, LOW); pinMode(PIN_XM, INPUT);      // Hi-Z X-
-  digitalWrite(PIN_YP, LOW); pinMode(PIN_YP, INPUT);      // Hi-Z Y+
+  digitalWrite(PIN_XM, LOW);
+  pinMode(PIN_XM, INPUT);   // Hi-Z X-
+  digitalWrite(PIN_YP, LOW);
+  pinMode(PIN_YP, INPUT);   // Hi-Z Y+
 
   int z1 = analogRead(PIN_XM);
   int z2 = 1023 - analogRead(PIN_YP);
@@ -126,27 +133,29 @@ uint16_t myPressure(void) {
 // Note - For Griduino, if this function takes longer than 8 msec it can cause erratic GPS readings
 // so we recommend against using https://forum.arduino.cc/index.php?topic=449719.0
 bool TouchScreen::isTouching(void) {
-  #define TOUCHPRESSURE 200       // Minimum pressure we consider true pressing
+#define TOUCHPRESSURE 200   // Minimum pressure we consider true pressing
   static bool button_state = false;
   uint16_t pres_val        = ::myPressure();
 
   if ((button_state == false) && (pres_val > TOUCHPRESSURE)) {
-    Serial.print(". pressed, pressure = "); Serial.println(pres_val);     // debug
+    Serial.print(". pressed, pressure = ");
+    Serial.println(pres_val);   // debug
     button_state = true;
   }
 
   if ((button_state == true) && (pres_val < TOUCHPRESSURE)) {
-    Serial.print(". released, pressure = "); Serial.println(pres_val);       // debug
+    Serial.print(". released, pressure = ");
+    Serial.println(pres_val);   // debug
     button_state = false;
   }
 
   // Clean the touchScreen settings after function is used
   // Because LCD may use the same pins
   // todo - is this actually necessary?
-  //pinMode(_xm, OUTPUT);     digitalWrite(_xm, LOW);
-  //pinMode(_yp, OUTPUT);     digitalWrite(_yp, HIGH);
-  //pinMode(_ym, OUTPUT);     digitalWrite(_ym, LOW);
-  //pinMode(_xp, OUTPUT);     digitalWrite(_xp, HIGH);
+  // pinMode(_xm, OUTPUT);     digitalWrite(_xm, LOW);
+  // pinMode(_yp, OUTPUT);     digitalWrite(_yp, HIGH);
+  // pinMode(_ym, OUTPUT);     digitalWrite(_ym, LOW);
+  // pinMode(_xp, OUTPUT);     digitalWrite(_xp, HIGH);
 
   return button_state;
 }
